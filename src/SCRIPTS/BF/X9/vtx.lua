9223059e45ca6849146a749c07892519e1142309
Curtis Bangert <codecae@gmail.com>
2017-10-22 16:33:55 -0400

    Updated VTX logic to ignore trailing msp bytes on writes

--- src/SCRIPTS/BF/X9/vtx.lua
+++ src/SCRIPTS/BF/X9/vtx.lua
@@ -34,7 +34,7 @@ return {
         local channel = (self.values[2]-1)*8 + self.values[3]-1
         self.values[2] = bit32.band(channel,0xFF)
         self.values[3] = bit32.rshift(channel,8)
-        for i=2,#(self.minBytes) do
+        for i=2,self.minBytes do
             valsTemp[i-1] = self.values[i]
         end
         self.values = valsTemp
