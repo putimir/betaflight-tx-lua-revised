1176c61f9ca70859b1a42a1a3528f8dbe95bcf96
putimir <33059362+putimir@users.noreply.github.com>
2017-11-06 17:03:06 +0100

    Update pwm.lua
    Minor layout update to better fit Horus screens

--- src/SCRIPTS/BF/X12S/pwm.lua
+++ src/SCRIPTS/BF/X12S/pwm.lua
@@ -6,22 +6,22 @@ return {
     title             = "PWM",
     minBytes             = 9,
     text= {
-        { t = "32K", x = 48, y = 14, to = SMLSIZE },
-        { t = "Gyro Rt", x = 29, y = 24, to = SMLSIZE },
-        { t = "PID Rt", x = 35, y = 34, to = SMLSIZE },
-        { t = "Protocol", x = 107, y = 14, to = SMLSIZE },
-        { t = "Unsynced", x = 106, y = 24, to = SMLSIZE },
-        { t = "PWM Rate", x = 105, y = 34, to = SMLSIZE },
-        { t = "Idle Offset", x =94, y = 44, to = SMLSIZE }
+        { t = "Protocol", x = 50, y = 65, to = SMLSIZE },
+        { t = "32K", x = 50, y = 110, to = SMLSIZE },
+        { t = "Gyro Rt", x = 50, y = 155, to = SMLSIZE },
+        { t = "PID Rt", x = 50, y = 200, to = SMLSIZE },
+        { t = "Unsynced", x = 240, y = 110, to = SMLSIZE },
+        { t = "PWM Rate", x = 240, y = 155, to = SMLSIZE },
+        { t = "Idle Offset", x = 240, y = 200, to = SMLSIZE }
     },
     fields = {
-        { x = 65, y = 14, vals = { 9 }, min = 0, max = 1, to = SMLSIZE, table = { [0] = "OFF", "ON" }, upd = function(self) self.updateRateTables(self) end },
-        { x = 65, y = 24, vals = { 1 }, min = 1, max = 32, to = SMLSIZE, upd = function(self) self.updatePidRateTable(self) end },
-        { x = 65, y = 34, vals = { 2 }, min = 1, max = 16, to = SMLSIZE, },
-        { x = 148, y = 14, vals = { 4 }, min = 0, max = 9, to = SMLSIZE, table = { [0] = "OFF", "ONESHOT125", "ONESHOT42", "MULTISHOT","BRUSHED", "DSHOT150", "DSHOT300", "DSHOT600","DSHOT1200", "PROSHOT1000" } },
-        { x = 148, y = 24, vals = { 3 }, min = 0, max = 1, to = SMLSIZE, table = { [0] = "OFF", "ON" } },
-        { x = 148, y = 34, vals = { 5, 6 }, min = 200, max = 32000, to = SMLSIZE },
-        { x = 148, y = 44, vals = { 7, 8 }, min = 0, max = 2000, to = SMLSIZE, scale = 100 },
+        { x = 130, y = 55, vals = { 4 }, min = 0, max = 9, to = MIDSIZE, table = { [0] = "OFF", "ONESHOT125", "ONESHOT42", "MULTISHOT","BRUSHED", "DSHOT150", "DSHOT300", "DSHOT600","DSHOT1200", "PROSHOT1000" } },
+        { x = 130, y = 95, vals = { 9 }, min = 0, max = 1, to = DBLSIZE, table = { [0] = "OFF", "ON" }, upd = function(self) self.updateRateTables(self) end },
+        { x = 130, y = 140, vals = { 1 }, min = 1, max = 32, to = DBLSIZE, upd = function(self) self.updatePidRateTable(self) end },
+        { x = 130, y = 185, vals = { 2 }, min = 1, max = 16, to = DBLSIZE, },
+        { x = 350, y = 95, vals = { 3 }, min = 0, max = 1, to = DBLSIZE, table = { [0] = "OFF", "ON" } },
+        { x = 350, y = 140, vals = { 5, 6 }, min = 200, max = 32000, to = DBLSIZE },
+        { x = 350, y = 185, vals = { 7, 8 }, min = 0, max = 2000, to = DBLSIZE, scale = 100 },
     },
     calculateGyroRates = function(self, baseRate)
         self.gyroRates = {}
