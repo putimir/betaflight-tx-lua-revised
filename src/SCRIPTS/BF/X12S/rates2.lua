f9f35e55bbfe9003aba3902f65f20a5110ae3627
putimir <33059362+putimir@users.noreply.github.com>
2017-11-06 17:10:32 +0100

    Update rates2.lua
    Minor layout update to better fit Horus screens

--- src/SCRIPTS/BF/X12S/rates2.lua
+++ src/SCRIPTS/BF/X12S/rates2.lua
@@ -8,23 +8,23 @@ return {
    minBytes       = 23,
    text = {
       { t = "Anti-Gravity",       x =  35, y =  68, to = NORMSIZE },
-      { t = "Gain",               x =  35, y =  96, to = SMLSIZE },
-      { t = "Threshold",          x =  35, y = 124, to = SMLSIZE },
-      { t = "Dterm Setpoint",     x = 200, y =  68, to = NORMSIZE },
-      { t = "Weight",             x = 200, y =  96, to = SMLSIZE },
-      { t = "Transition",         x = 200, y = 124, to = SMLSIZE },
-      { t = "VBAT Compensation",  x =  35, y = 172, to = SMLSIZE }
+      { t = "Gain",               x =  35, y = 115, to = SMLSIZE },
+      { t = "Threshold",          x =  35, y = 155, to = SMLSIZE },
+      { t = "Dterm Setpoint",     x = 240, y = 68,  to = NORMSIZE },
+      { t = "Weight",             x = 240, y = 115, to = SMLSIZE },
+      { t = "Transition",         x = 240, y = 155, to = SMLSIZE },
+      { t = "VBAT Compensation",  x =  35, y = 200, to = SMLSIZE }
    },
    fields = {
       --  GAIN
-      { x = 129, y =  96, min = 1000, max = 30000, vals = { 22, 23 }, scale = 1000, mult = 1000, to = SMLSIZE },
+      { x = 129, y =  100, min = 1000, max = 30000, vals = { 22, 23 }, scale = 1000, mult = 1000, to = DBLSIZE },
       --  THRESHOLD
-      { x = 129, y = 124, min = 20, max = 1000, vals = { 20, 21 }, to = SMLSIZE },
+      { x = 129, y = 140, min = 20, max = 1000, vals = { 20, 21 }, to = DBLSIZE },
       --  WEIGHT
-      { x = 300, y =  96, min = 0, max = 254, vals = { 10 }, scale = 100, to = SMLSIZE },
+      { x = 340, y =  100, min = 0, max = 254, vals = { 10 }, scale = 100, to = DBLSIZE },
       --  TRANSITION
-      { x = 300, y = 124, min = 0, max = 100, vals = { 9 }, scale = 100, to = SMLSIZE },
+      { x = 340, y = 140, min = 0, max = 100, vals = { 9 }, scale = 100, to = DBLSIZE },
       --  VBAT COMPENSATION
-      { x = 200, y = 172, min = 0, max = 1, vals = { 8 }, table = { [0]="OFF", "ON" }, to = SMLSIZE },
+      { x = 240, y = 185, min = 0, max = 1, vals = { 8 }, table = { [0]="OFF", "ON" }, to = DBLSIZE },
    }
 }
